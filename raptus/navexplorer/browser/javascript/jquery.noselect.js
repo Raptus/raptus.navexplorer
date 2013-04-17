af0fdee0400c3dfee25f983a96b25106a0cfae35
cmarangon <cmarangon@raptus.com>
2013-04-17 17:28:26 +0200

    new feature 'dragging' & bugfixes

---
+++ raptus/navexplorer/browser/javascript/jquery.noselect.js
@@ -0,0 +1,14 @@
+(function($) {
+    $.fn.disableTextSelect = function() {
+        return this.each(function(){
+            if($.browser.mozilla){//Firefox
+                $(this).css('MozUserSelect','none');
+            }else if($.browser.msie){//IE
+
+                $(this).bind('selectstart',function(){return false;});
+            }else{//Opera, etc.
+                $(this).mousedown(function(){return false;});
+            }
+        });
+    }
+})(jQuery);
